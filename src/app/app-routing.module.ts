f8fce523cf43c286842df6cd1bef1a9806d1a379
riaz <riazasim111@gmail.com>
2021-06-11 13:36:53 +0200

    routing added to app routig module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './app-components/login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '**',
+    redirectTo: ''
+  }
+    
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -5,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './app-components/navigation/navigation.component';
 import { BannerComponent } from './app-components/banner/banner.component';
+import { LoginComponent } from './app-components/login/login.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    BannerComponent
+    BannerComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
